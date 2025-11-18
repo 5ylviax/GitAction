a16ddfaf460d81c21ca1da2c8f6025d8afefabb4
5ylviax <silviasaavedra97@gmail.com>
2025-11-18 10:12:11 -0600

    test 2

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,12 @@
 def count_words(self, text):
-    pass
+    words = text.split()
+    word_count = len(words)
+    return word_count
+
 def count_chars(self, text):
-    pass 
+    my_string = text
+    count_a = my_string.count("a")
+    return count_a
+
 def find_most_common_word(self, text):
-    pass
+    pass 
