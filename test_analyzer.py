e8db3669e797d1cf1e8a89253c10c0b53ed0a50e
5ylviax <silviasaavedra97@gmail.com>
2025-11-18 13:15:59 -0600

    updated assert

--- test_analyzer.py
+++ test_analyzer.py
@@ -3,7 +3,7 @@ from analyzer import count_words, count_chars, find_most_common_word
 
 def test_count_words():
     # empty string 
-    assert count_words("") is None
+    assert count_words("") is 0
     assert count_words("Hello world") == "Hello world"
     assert count_words("One two three four") == "One two three four"
     with pytest.raises(ValueError):
@@ -12,13 +12,14 @@ def test_count_words():
 def test_count_chars():
     assert count_chars('c') == 'c'
     assert count_chars('hello') == 'hello'
+    assert count_chars('') == 0
     with pytest.raises(ValueError):
         count_chars(3)
 
 def test_find_most_common_word():
     assert find_most_common_word("apple banana apple") == "apple"
     assert find_most_common_word("one two two three three three") == "three"
-    assert find_most_common_word("") is None
+    assert find_most_common_word("") is 0
     with pytest.raises("ValueError"):
         find_most_common_word(456)
         
